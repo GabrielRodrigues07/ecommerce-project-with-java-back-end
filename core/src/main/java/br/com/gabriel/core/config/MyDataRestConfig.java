a71dde602da54316fd93ef3e403759cf85bffcbf
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-06-20 20:37:38 -0300

    feat(S09-#20/06/24) bloqueando requisições para PUT,POST e DELETE

---
+++ core/src/main/java/br/com/gabriel/core/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package br.com.gabriel.core.config;
+
+import br.com.gabriel.ecommerce.model.domain.entity.Product;
+import br.com.gabriel.ecommerce.model.domain.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PUT};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+
+}
